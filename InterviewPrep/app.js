15c645919a8a8ef42695a2d459870ec3a8395315
La_Professor_De_BellaCiao <wofford.codes@gmail.com>
2022-10-31 06:18:57 -0500

    updates

---
+++ InterviewPrep/app.js
@@ -0,0 +1,22 @@
+let subs = [
+    "Big O Notation",
+    "Linked Lists",
+    "Stacks",
+    "Queues",
+    "Heaps[Min/Max]",
+    "Selection Sort Algo",
+    "Insertion Sort Algo",
+    "Merge Sort",
+    "Quick Sort",
+    "Binary Search Trees",
+    "Balanced Search Trees",
+    "Bredth First Search",
+    "Depth First Search",
+    "API Gateway",
+    "PROXIES/ REVERSE PROXIES",
+    "Concurrency",
+    "Objects & Pointers",
+    "Matrix",
+    "Adjaceny List"
+
+]
