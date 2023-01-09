4ff1cb17ebb274c5fd3502096452061a3d52db90
woffordCodesAdmin <wofford.codes@gmail.com>
2023-01-09 05:25:03 -0600

    updated Resume exercise

---
+++ Interactive_Resume/app.js
@@ -0,0 +1,48 @@
+
+// Interactive Resume Mockup:
+// Choose your resume website template or use the template of an existing website.
+
+// Add a professional picture of yourself.
+// Add the relevant sections [Education,Industry Overview, Work Experience].
+// Add in the descriptive elements
+
+// Optimize for SEO. {Keywords to Target Job Search Algos.}
+
+// Make sure you're mobile-friendly. {* Learn about Responsive Web Design} 
+
+//  Data Structure For Resume: 
+
+// Object Breakdown:
+
+// let resume = {
+//     jordanWofford: {
+//         name: "",
+//         email: "",
+//         contact: "",
+//         bio: [""],
+//         keywords: ["", "", ""],
+//         imageUrl: "",
+//         education: {
+//             universities: [],
+//             notableCerts: [],
+//             majors: [],
+//             minors: [],
+//             athletics: []
+//         },
+//         music: {
+//             songOne: {
+//                 artist: '',
+//                 song: '',
+//                 songUrl: ''
+//             }, songTwo: {
+//                 artist: '',
+//                 song: '',
+//                 songUrl: ''
+//             }, songThree: {
+//                 artist: '',
+//                 song: '',
+//                 songUrl: ''
+//             }
+//         }
+//     }
+// }
